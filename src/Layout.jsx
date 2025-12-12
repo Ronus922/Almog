36751511f59e308b45108b42418933516b999f27
Ronen Meshulam <r@bios.co.il>
2025-12-12 19:32:06 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -204,7 +204,7 @@ export default function Layout({ children, currentPageName }) {
           letter-spacing: -0.01em;
         }
         html {
-          font-size: 18px;
+          font-size: 19px;
         }
         body {
           font-weight: 400;
