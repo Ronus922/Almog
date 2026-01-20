10d3ca97d771c4a626651f14e3edafc26f04c0bc
Ronen Meshulam <r@bios.co.il>
2026-01-20 17:32:07 +0000

    File changes

--- functions/sendStatusChangeNotification.ts
+++ functions/sendStatusChangeNotification.ts
@@ -105,7 +105,7 @@ Deno.serve(async (req) => {
                 console.log(`[EMAIL] Sending to: ${email}...`);
                 
                 const { data, error } = await resend.emails.send({
-                    from: 'התראות <onboarding@resend.dev>',
+                    from: 'התראות <r@bios.co.il>',
                     to: [email],
                     subject: subject,
                     html: htmlContent,
