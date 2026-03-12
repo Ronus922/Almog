77bba52fc5435dff95e662be2c280b1c3b0ca022
Ronen Meshulam <r@bios.co.il>
2026-03-12 20:32:21 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
 
+import Tasks from "./pages/Tasks";
 const { Pages, Layout, mainPage } = pagesConfig;
 const mainPageKey = mainPage ?? Object.keys(Pages)[0];
 const MainPage = mainPageKey ? Pages[mainPageKey] : <></>;
