72f069598a76d6300b808f60bfd166cbb034d3a9
Ronen Meshulam <r@bios.co.il>
2026-03-13 01:14:05 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -255,6 +255,7 @@ export default function Layout({ children, currentPageName }) {
   return (
     <AuthProvider>
       <ImportProvider>
+        <AlertProvider>
         <style>{`
         @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+Hebrew:wght@300;400;500;600;700;800;900&display=swap');
 
