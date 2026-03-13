dad808d156be69f7e3782d754b960ae47d12c789
Ronen Meshulam <r@bios.co.il>
2026-03-13 01:03:23 +0000

    File changes

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -379,8 +379,7 @@ export default function Tasks() {
                           {formatDateTime(task.created_date)}
                         </td>
                       </tr>
-                      );
-                    })}
+                      ))}
                   </tbody>
                 </table>
               </div>
