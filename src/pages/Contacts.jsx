1eb7b30b3761089af1d9f1425a0ae26368ea9f68
Ronen Meshulam <r@bios.co.il>
2026-03-14 08:44:47 +0000

    File changes

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -4,7 +4,6 @@ import { base44 } from "@/api/base44Client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Badge } from "@/components/ui/badge";
-import { Card, CardContent } from "@/components/ui/card";
 import { Checkbox } from "@/components/ui/checkbox";
 import {
   Table,
