018ada3a410cce81319a7199e1fd077616a3ed8f
Ronen Meshulam <r@bios.co.il>
2026-03-14 08:41:28 +0000

    File changes

--- src/pages/UserManagement.jsx
+++ src/pages/UserManagement.jsx
@@ -479,6 +479,7 @@ export default function UserManagement() {
           onSave={handleSaveEditedUser}
           existingUsernames={users.map(u => u.username)}
         />
+        </div>
       </div>
     </div>
   );
