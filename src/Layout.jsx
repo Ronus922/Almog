0409e043f33fcc7adce088877e672ffa5f3e204a
Ronen Meshulam <r@bios.co.il>
2026-03-15 22:43:27 +0000

    Manual code change

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -133,9 +133,7 @@ function LayoutContent({ children, currentPageName }) {
         </button>
 
         {/* Search/Filter */}
-        {!isCollapsed &&
-        <div className="px-3 py-2.5 border-b border-slate-700">
-            
+        
 
 
 
@@ -145,8 +143,10 @@ function LayoutContent({ children, currentPageName }) {
 
 
 
-          </div>
-        }
+
+
+
+
 
         {/* Notifications Section */}
         <div className="px-3 py-2 border-b border-slate-700">
