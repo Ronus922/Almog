ff9176c36bae876eefdae686fef7e4cdf921c9fb
Ronen Meshulam <r@bios.co.il>
2026-03-15 21:39:29 +0000

    File changes

--- src/components/todo/TodoItemForm.jsx
+++ src/components/todo/TodoItemForm.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
+import { X } from 'lucide-react';
 import { base44 } from '@/api/base44Client';
 import { useQuery } from '@tanstack/react-query';
 
